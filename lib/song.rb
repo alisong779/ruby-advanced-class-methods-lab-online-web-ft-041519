eb53ffa349e56cc514d9688441075e1d3127bee1
Alison Golden <alisong779@yahoo.com>
2019-04-23 19:40:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
     song 
   end 
 
- def self.new_from_filename(filename)
+ def self.create_from_filename(filename)
     new_name = filename.slice(0, filename.length-4).split(" - ")
     song = self.new 
     song.name = new_name[1]   #second element - song name 
